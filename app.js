1437f59d5a2658bbb78801823e19ae68e15693a2
ivan2798 <inge1998@hotmail.com>
2022-09-19 12:17:03 -0500

    casting

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let a = 2;
 let b = new Number(1);
-let c = 7.19
+let c = 7.19;
+let d = "5.6";
 
 console.log(a,b);
 console.log(c.toFixed(1));
 console.log(c.toFixed(1));
+console.log(parseInt(c));
+console.log(parseFloat(c));
+console.log(typeof c, typeof d);
+console.log(a+b);
+console.log(c+d);
